6f4c359da35ff64592c7eb15b3c90c7a533c148a
Emre <emre.bas@ogr.bandirma.edu.tr>
2024-07-23 22:40:10 +0300

    Roles created

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectOneMil.Models;
+
+namespace ProjectOneMil.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View(_roleManager.Roles);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(AppRole model)
+        {
+            if (ModelState.IsValid) {
+                var result = await _roleManager.CreateAsync(model);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
