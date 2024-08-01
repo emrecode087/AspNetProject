21287bf0d2dfd1aecaa11b14404cfae89ba074b8
Emre <emre.bas@ogr.bandirma.edu.tr>
2024-08-01 16:05:11 +0300

    excel export completed

--- Controllers/MyTableController.cs
+++ Controllers/MyTableController.cs
@@ -5,6 +5,8 @@ using ProjectOneMil.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Linq.Dynamic.Core;
+using OfficeOpenXml;
+using System.IO;
 
 namespace ProjectOneMil.Controllers
 {
@@ -137,6 +139,84 @@ namespace ProjectOneMil.Controllers
             _context.onemildata.Remove(myTable);
             _context.SaveChanges();
             return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Download()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Lisans bağlamını ayarlayın
+
+            var data = await _context.onemildata.ToListAsync();
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("MyTableData");
+                worksheet.Cells.LoadFromCollection(data, true);
+
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                string excelName = $"MyTableData-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Import(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            try
+            {
+                using (var stream = new MemoryStream())
+                {
+                    await file.CopyToAsync(stream);
+                    using (var package = new ExcelPackage(stream))
+                    {
+                        var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                        if (worksheet == null)
+                        {
+                            return BadRequest("No worksheet found in the Excel file.");
+                        }
+
+                        var rowCount = worksheet.Dimension.Rows;
+                        var myTableList = new List<MyTable>();
+
+                        for (int row = 2; row <= rowCount; row++)
+                        {
+                            var myTable = new MyTable
+                            {
+                                Column1 = worksheet.Cells[row, 1].Text,
+                                Column2 = int.Parse(worksheet.Cells[row, 2].Text),
+                                Column3 = decimal.Parse(worksheet.Cells[row, 3].Text),
+                                Column4 = DateTime.Parse(worksheet.Cells[row, 4].Text),
+                                Column5 = bool.Parse(worksheet.Cells[row, 5].Text),
+                                Column6 = worksheet.Cells[row, 6].Text,
+                                Column7 = worksheet.Cells[row, 7].Text,
+                                Column8 = int.Parse(worksheet.Cells[row, 8].Text),
+                                Column9 = decimal.Parse(worksheet.Cells[row, 9].Text),
+                                Column10 = Guid.Parse(worksheet.Cells[row, 10].Text)
+                            };
+                            myTableList.Add(myTable);
+                        }
+
+                        _context.onemildata.AddRange(myTableList);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error occurred while processing the file: {ex.Message}");
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
